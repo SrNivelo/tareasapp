9a23f5074b926cc444b3206b5bc5dc4af242aebf
Giusseppe Nivelo <giusseppenivelo@gmail.com>
2026-01-15 08:06:17 -0500

    Se añadió la función listar_tareas que permite visualizar las tareas añadidas, si no hay tareas se muestra un mensaje de error

--- tareas.py
+++ tareas.py
@@ -12,3 +12,18 @@ class GestorTareas:
             "completada": False
         })
         print("Tarea agregada correctamente.")
+
+    def listar_tareas(self):
+        # Esta función muestra todas las tareas que existen
+
+        if not self.tareas:
+            # Si no hay tareas en la lista, avisa al usuario
+            print("No hay tareas registradas.")
+            return
+
+        print("\nLISTA DE TAREAS:")
+        # Recorremos la lista de tareas y las mostramos una por una
+        for i, tarea in enumerate(self.tareas):
+            # Si la tarea está completada mostramos un símbolo distinto
+            estado = "✔" if tarea["completada"] else "✘"
+            print(f"{i+1}. {tarea['descripcion']} [{estado}]")
