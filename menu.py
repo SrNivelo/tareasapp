8cf2070a46430f66fe8d81a530cd97fac7683f96
Diego Pinto <diegofer.pintop@gmail.com>
2026-01-15 18:59:20 -0500

    Se creo la funcionalidad del menu con la respectiva función mostrar_menu que imprime en pantalla las opciones del menú

---
+++ menu.py
@@ -0,0 +1,9 @@
+class Menu:
+    def mostrar_menu(self):
+        # Esta función solo muestra las opciones del programa en pantalla
+
+        print("\n===== GESTOR DE TAREAS =====")
+        print("1. Agregar tarea")
+        print("2. Listar tareas")
+        print("3. Completar tarea")
+        print("4. Salir")
