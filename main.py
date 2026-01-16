1b8269cead244f3d13ca30f41b42eee5ae5664e4
Giusseppe Nivelo <giusseppenivelo@gmail.com>
2026-01-15 19:15:29 -0500

    Se unió todo el programa en una sola rama y se creó el archivo main para llamar a todas las funciones

---
+++ main.py
@@ -0,0 +1,9 @@
+from tareas import GestorTareas
+from estados import GestorEstados
+from menu import Menu
+
+def main():
+    # Creamos los objetos principales del programa
+    gestor = GestorTareas()
+    estados = GestorEstados()
+    menu = Menu()
